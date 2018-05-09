ed646beff41e4bb24d72db8087e83b7b6f294176
Marcela Ribeiro de Oliveira <mro15@inf.ufpr.br>
2018-05-09 16:02:12 -0300

    começando filtro

---
+++ include/playunfair.h
@@ -0,0 +1,6 @@
+#include "playfair.h"
+#define ACCEPTED 1
+#define REJECTED 0
+
+int filter(std::string text, const std::string key);
+void parser(std::string &text);
--- playunfair.cpp
+++ playunfair.cpp
@@ -1,23 +1,20 @@
-#include "include/playfair.h"
+#include "include/playunfair.h"
 #include <fstream>
 
-void parser(std::string &text){
-	text.erase(std::remove(text.begin(), text.end(), ' '), text.end());
-	std::transform(text.begin(), text.end(), text.begin(), ::tolower);
-}
 
 int main(int argc, char *argv[]){
-	if(argc!=3){
-		std::cout << "Uso: ./playunfair <file-in> <file-out>" << std::endl;
+	if(argc!=4){
+		std::cout << "Uso: ./playunfair <file-in> <file-out> <dicionarion>" << std::endl;
 		return 0;
 	}
-	std::fstream input, output;
-	char *inputName, *outputName;
-	std::string aux="", key = "cachorro", crip; //a key vai vir do dicionario futuramente
-	std::ostringstream text;
+	std::fstream input, output, dict;
+	char *inputName, *outputName, *dictName;
+	std::string aux="", key, crip; //a key vai vir do dicionario futuramente
+	std::ostringstream text, keys;
 
 	inputName = argv[1];
 	outputName = argv[2];
+	dictName = argv[3];
 
 	input.open(inputName, std::ifstream::in);
 	if(!input.good()){
@@ -32,10 +29,37 @@ int main(int argc, char *argv[]){
 	std::cout<<text.str()[2] << std::endl;
 	crip = text.str();
 	parser(crip);
-	std::cout << crip << std::endl;
-	crip = playfair(crip, key, CRYPT);
-	std::cout << crip << std::endl;
+	//std::cout << crip << std::endl;
 
+	dict.open(dictName, std::ifstream::in);
+	if(!dict.good()){
+		std::cout << "Nao foi possivel abrir o dicionario" << std::endl;
+		return 0;
+	}
+
+	//TODO: if filter returns accepted print key and text in file
+	while(std::getline(dict, aux)){
+		keys << aux;
+		key = keys.str();
+		crip = playfair(crip, key, DECRYPT);
+		std::cout << crip << std::endl;
+		std::cout << key << std::endl;
+		keys.str("");
+		keys.clear();
+		filter(crip, key); //chamar filtro para cada key
+	}
+
+	//crip = playfair(crip, key, DECRYPT);
+	//std::cout << crip << std::endl;
+	return 0;
+}
+
+void parser(std::string &text){
+	text.erase(std::remove(text.begin(), text.end(), ' '), text.end());
+	std::transform(text.begin(), text.end(), text.begin(), ::tolower);
+}
+
+int filter(std::string text, const std::string key){
 	//TODO: if ( 4 primeiras letras sem vogal ||
 	//           3 char -('o') iguais seguidos ||
 	//           5 consoantes ||
@@ -46,7 +70,5 @@ int main(int argc, char *argv[]){
 	//          )
 	//         { elimina texto: muda pra proxima chave}
 
-	crip = playfair(crip, key, DECRYPT);
-	std::cout << crip << std::endl;
-	return 0;
+	return ACCEPTED;
 }
